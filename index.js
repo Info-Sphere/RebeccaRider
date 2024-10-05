7238c877ac8f450f6db21d7a5f991af7c59a016b
Info-Sphere <165199424+Info-Sphere@users.noreply.github.com>
2024-10-05 12:44:45 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const navbarEl = document.querySelector(".navbar");
+
+
+const bottomContainerE1 = document.querySelector(".bottom-container");
+
+console.log(navbarEl.offsetHeight)
+
+console.log(bottomContainerE1.offsetTop);
+
+window.addEventListener("scroll", ()=> {
+    if(window.scrollY > bottomContainerE1.offsetTop - navbarEl.offsetHeight - 50){
+        navbarEl.classList.add("active")
+    }else{
+        navbarEl.classList.remove("active"); 
+    }
+});
+
