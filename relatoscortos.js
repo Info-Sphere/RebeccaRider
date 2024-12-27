3bd5a85e538c4ddce1c015d14d350603d1725420
Info-Sphere <165199424+Info-Sphere@users.noreply.github.com>
2024-12-27 22:35:19 +0100

    Update relatoscortos.js

--- relatoscortos.js
+++ relatoscortos.js
@@ -5,7 +5,7 @@ document.querySelectorAll('.interactive-list li a').forEach(link => {
         const listItem = this.closest('li'); // Find the list item
         listItem.classList.add('clicked');  // Add the 'clicked' class to trigger CSS transition
         
-        // Wait for the transition to finish (300ms delay)
+        // Wait for the transition to finish (400ms delay)
         setTimeout(() => {
             window.open(this.href, '_blank');  // Open the link in a new tab
         }, 300);  // 300ms delay (same as the transition duration)
