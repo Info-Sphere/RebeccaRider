c1ba99f18a2461ff2d16efc5a27149b9706e7d88
Info-Sphere <165199424+Info-Sphere@users.noreply.github.com>
2025-01-12 23:20:46 +0100

    Add files via upload

--- relatoscortos.js
+++ relatoscortos.js
@@ -1,3 +1,4 @@
+
 document.querySelectorAll('.interactive-list li a').forEach(link => {
     link.addEventListener('click', function (e) {
         e.preventDefault();  // Prevent immediate opening of the link
@@ -5,7 +6,7 @@ document.querySelectorAll('.interactive-list li a').forEach(link => {
         const listItem = this.closest('li'); // Find the list item
         listItem.classList.add('clicked');  // Add the 'clicked' class to trigger CSS transition
         
-        // Wait for the transition to finish (400ms delay)
+        // Wait for the transition to finish (300ms delay)
         setTimeout(() => {
             window.open(this.href, '_blank');  // Open the link in a new tab
         }, 300);  // 300ms delay (same as the transition duration)
